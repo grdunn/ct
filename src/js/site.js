d63d4fb285d6eea5011f40122097a3f8f1e3ccde
Gregory Dunn <grdunn@gmail.com>
2020-03-20 15:16:02 -0400

    up to date bar graph data

--- src/js/site.js
+++ src/js/site.js
@@ -25,10 +25,10 @@ function bindEvents () {
   Chart.defaults.global.legend.display = false;
   const $components = $(component);
   $(refreshTrigger).on('click', function () {
-    refresh($components, 'all');
+    refresh($components);
   });
   // setFilters();
-  refresh($components, 'all');
+  refresh($components);
 };
 
 // function setFilters () {
@@ -55,7 +55,144 @@ function bindEvents () {
 //   })
 // }
 
-function run (set, query) {
+// function run (set, query) {
+//   let state = $(set).data().state;
+//   let tableBody = $(set).find(body);
+//   let d = new Date();
+//   let month = d.getMonth() + 1;
+//   let date = d.getDate();
+//   let year = d.getFullYear();
+//   let hours = d.getHours();
+//   let min = d.getMinutes();
+//   let url = 'https://covidtracking.com/api/';
+
+
+  // Only necessary if we do filters
+  // if (query === 'all') {
+  //   url += `states/daily?state=${state}`;
+  // } else {
+  //   url += `states/daily?state=${state}&date=${query}`;
+  // }
+
+  // url = states/daily?state=${state}
+
+//   $(refreshData).empty().append(`Last refresh: <strong>${month}/${date}/${year} ${hours}:${min}</strong>`)
+//
+//   $.ajax({
+//     url: url,
+//     success: function(result) {
+//       let dataset
+//       let everything = false;
+//       if (result.length && result.length > 1) {
+//         dataset = result[0];
+//         everything = true;
+//       } else {
+//         dataset = result;
+//       }
+//
+//       let dateCheck = moment(dataset.dateChecked).format('MM/DD/YY');
+//
+//       $(tableBody).empty().append(
+//         `<tr>
+//           <td>${dataset.positive}</td>
+//           <td>${dataset.negative}</td>
+//           <td class='desktop'>${dataset.pending}</td>
+//           <td>${dataset.death}</td>
+//           <td>${dataset.total}</td>
+//           <td class='desktop'>${dateCheck}</td>
+//         </tr>`
+//       );
+//
+//       let lineChartLabels = [];
+//       let lineChartPositiveData = [];
+//       let lineChartNegativeData = [];
+//       let lineChartDeathData = [];
+//
+//       if (everything) {
+//
+//         result.forEach(function (data, i) {
+//           lineChartLabels.push(moment(data.dateChecked).format('MM-DD'));
+//           lineChartPositiveData.push(data.positive)
+//           lineChartNegativeData.push(data.negative)
+//           lineChartDeathData.push(data.death)
+//         });
+//
+//         let ctxLine = document.getElementById(state + '-LINE').getContext('2d');
+//         var myLineChart = new Chart(ctxLine, {
+//           type: 'line',
+//           data: {
+//             labels: lineChartLabels.reverse(),
+//             datasets: [
+//               {
+//                 label: 'Positive',
+//                 data: lineChartPositiveData.reverse(),
+//                 borderColor: [
+//                     'rgba(255, 99, 132, 0.2)'
+//                 ],
+//                 borderWidth: 3,
+//                 fill: false
+//               },
+//               {
+//                 label: 'Negative',
+//                 data: lineChartNegativeData.reverse(),
+//                 borderColor: [
+//                     'rgba(54, 162, 235, 0.2)'
+//                 ],
+//                 borderWidth: 3,
+//                 fill: false
+//               },
+//               {
+//                 label: 'Death',
+//                 data: lineChartDeathData.reverse(),
+//                 borderColor: [
+//                     'rgba(255, 206, 86, 0.2)'
+//                 ],
+//                 borderWidth: 3,
+//                 fill: false
+//               }
+//             ]
+//           }
+//         });
+//       }
+//
+//       let ctx = document.getElementById(state).getContext('2d');
+//       const myChart = new Chart(ctx, {
+//           type: 'horizontalBar',
+//           data: {
+//               labels: ['Positive', 'Negative', 'Deaths', 'Total'],
+//               datasets: [{
+//                 label: state,
+//                 data: [
+//                   dataset.positive,
+//                   dataset.negative,
+//                   dataset.death,
+//                   dataset.total
+//                 ],
+//                 backgroundColor: [
+//                     'rgba(255, 99, 132, 0.2)',
+//                     'rgba(54, 162, 235, 0.2)',
+//                     'rgba(255, 206, 86, 0.2)',
+//                     'rgba(75, 192, 192, 0.2)'
+//                 ],
+//                 borderWidth: 1
+//               }
+//             ]
+//           },
+//           options: {
+//             scales: {
+//               yAxes: [{
+//                 ticks: {
+//                   beginAtZero: true
+//                 }
+//               }]
+//             }
+//           }
+//       });
+//     }
+//   });
+// }
+
+function runGraph (set) {
   let state = $(set).data().state;
   let tableBody = $(set).find(body);
   let d = new Date();
@@ -64,96 +201,21 @@ function run (set, query) {
   let year = d.getFullYear();
   let hours = d.getHours();
   let min = d.getMinutes();
-  let url = 'https://covidtracking.com/api/';
-
-  console.log(d);
-
-  if (query === 'all') {
-    url += `states/daily?state=${state}`;
-  } else {
-    url += `states/daily?state=${state}&date=${query}`;
-  }
-
-  $(refreshData).empty().append(`Last refresh: <strong>${month}/${date}/${year} ${hours}:${min}</strong>`)
-
+  let url = `https://covidtracking.com/api/states?state=${state}`;
+  let ctx = document.getElementById(state).getContext('2d');
   $.ajax({
     url: url,
     success: function(result) {
-      let dataset
-      let everything = false;
-      if (result.length && result.length > 1) {
-        dataset = result[0];
-        everything = true;
-      } else {
-        dataset = result;
-      }
-
-      let dateCheck = moment(dataset.dateChecked).format('MM/DD/YY');
-
       $(tableBody).empty().append(
         `<tr>
-          <td>${dataset.positive}</td>
-          <td>${dataset.negative}</td>
-          <td class='desktop'>${dataset.pending}</td>
-          <td>${dataset.death}</td>
-          <td>${dataset.total}</td>
-          <td class='desktop'>${dateCheck}</td>
+          <td>${result.positive}</td>
+          <td>${result.negative}</td>
+          <td class='desktop'>${result.pending}</td>
+          <td>${result.death}</td>
+          <td>${result.total}</td>
+          <td class='desktop'>${result.lastUpdateEt}</td>
         </tr>`
       );
-
-      let lineChartLabels = [];
-      let lineChartPositiveData = [];
-      let lineChartNegativeData = [];
-      let lineChartDeathData = [];
-
-      if (everything) {
-
-        result.forEach(function (data, i) {
-          lineChartLabels.push(moment(data.dateChecked).format('MM-DD'));
-          lineChartPositiveData.push(data.positive)
-          lineChartNegativeData.push(data.negative)
-          lineChartDeathData.push(data.death)
-        });
-
-        let ctxLine = document.getElementById(state + '-LINE').getContext('2d');
-        var myLineChart = new Chart(ctxLine, {
-          type: 'line',
-          data: {
-            labels: lineChartLabels.reverse(),
-            datasets: [
-              {
-                label: 'Positive',
-                data: lineChartPositiveData.reverse(),
-                borderColor: [
-                    'rgba(255, 99, 132, 0.2)'
-                ],
-                borderWidth: 3,
-                fill: false
-              },
-              {
-                label: 'Negative',
-                data: lineChartNegativeData.reverse(),
-                borderColor: [
-                    'rgba(54, 162, 235, 0.2)'
-                ],
-                borderWidth: 3,
-                fill: false
-              },
-              {
-                label: 'Death',
-                data: lineChartDeathData.reverse(),
-                borderColor: [
-                    'rgba(255, 206, 86, 0.2)'
-                ],
-                borderWidth: 3,
-                fill: false
-              }
-            ]
-          }
-        });
-      }
-
-      let ctx = document.getElementById(state).getContext('2d');
       const myChart = new Chart(ctx, {
           type: 'horizontalBar',
           data: {
@@ -161,10 +223,10 @@ function run (set, query) {
               datasets: [{
                 label: state,
                 data: [
-                  dataset.positive,
-                  dataset.negative,
-                  dataset.death,
-                  dataset.total
+                  result.positive,
+                  result.negative,
+                  result.death,
+                  result.total
                 ],
                 backgroundColor: [
                     'rgba(255, 99, 132, 0.2)',
@@ -186,13 +248,72 @@ function run (set, query) {
             }
           }
       });
+      $(refreshData).empty().append(`Last refresh: <strong>${month}/${date}/${year} ${hours}:${min}</strong>`)
     }
   });
-}
+};
+
+function runLine (set) {
+  let state = $(set).data().state;
+  let tableBody = $(set).find(body);
+  let url = `https://covidtracking.com/api/states/daily?state=${state}`
+  let lineChartLabels = [];
+  let lineChartPositiveData = [];
+  let lineChartNegativeData = [];
+  let lineChartDeathData = [];
+  let ctxLine = document.getElementById(state + '-LINE').getContext('2d');
+  $.ajax({
+    url: url,
+    success: function(result) {
+      result.forEach(function (data, i) {
+        lineChartLabels.push(moment(data.dateChecked).format('MM-DD'));
+        lineChartPositiveData.push(data.positive)
+        lineChartNegativeData.push(data.negative)
+        lineChartDeathData.push(data.death)
+      });
+      var myLineChart = new Chart(ctxLine, {
+        type: 'line',
+        data: {
+          labels: lineChartLabels.reverse(),
+          datasets: [
+            {
+              label: 'Positive',
+              data: lineChartPositiveData.reverse(),
+              borderColor: [
+                  'rgba(255, 99, 132, 0.2)'
+              ],
+              borderWidth: 3,
+              fill: false
+            },
+            {
+              label: 'Negative',
+              data: lineChartNegativeData.reverse(),
+              borderColor: [
+                  'rgba(54, 162, 235, 0.2)'
+              ],
+              borderWidth: 3,
+              fill: false
+            },
+            {
+              label: 'Death',
+              data: lineChartDeathData.reverse(),
+              borderColor: [
+                  'rgba(255, 206, 86, 0.2)'
+              ],
+              borderWidth: 3,
+              fill: false
+            }
+          ]
+        }
+      });
+    }
+  });
+};
 
 function refresh(components, query) {
   components.each(function(i, set) {
-    run(set, query);
+    runGraph(set);
+    runLine(set);
   });
 }
 
